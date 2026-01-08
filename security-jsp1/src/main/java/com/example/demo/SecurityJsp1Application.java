2f850a339d31b11236083826084445c6270a2da0
wawdse <aiwdoao@gmail.com>
2026-01-08 15:06:25 +0900

    시큐리티jsp버전

---
+++ security-jsp1/src/main/java/com/example/demo/SecurityJsp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJsp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJsp1Application.class, args);
+	}
+
+}
